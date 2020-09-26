deee1f0e3df55b162bb327d91a65a81d3abc06db
刘理 <liul@blacksteed.cn>
2020-09-26 14:52:47 +0800

    add service impl

---
+++ src/main/kotlin/test/SomeServiceImpl.kt
@@ -0,0 +1,11 @@
+package test
+
+import java.util.concurrent.CompletionStage
+
+class SomeServiceImpl : SomeService {
+
+    override fun doWork(`in`: SomeRequest?): CompletionStage<Result> {
+        TODO("Not yet implemented")
+    }
+
+}
